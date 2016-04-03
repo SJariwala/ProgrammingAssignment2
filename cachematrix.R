cef534cbbb20cbc5ef1461d22f8ba926f4e4248e
Shree <shree.jariwala@gmail.com>
2016-04-02 20:39:11 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,2 +1,30 @@
+## makeCacheMatrix creates a list to set the matrix value, get the matrix value, set the value of the matrix and then get the value. This code is confusing.
 
-test
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+}
+
+
+
+## cacheSolve first checks if the inverse exists. If it does, it prints it. If it doesn't, then it solves the matrix
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data.")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data,...)
+  x$setinverse(inv)
+  inv
+}
